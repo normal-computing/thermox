71738834b38934e59189d4b3f229d439b73f90eb
Sam Duffield <s@mduffield.com>
2024-01-30 14:32:44 +0000

    Move log_prob

--- thermox/__init__.py
+++ thermox/__init__.py
@@ -3,6 +3,9 @@ from thermox import linalg
 from thermox.sampler import collect_samples_identity_diffusion
 from thermox.sampler import collect_samples
 
+from thermox.log_prob import log_prob_identity_diffusion
+from thermox.log_prob import log_prob
+
 from thermox.utils import preprocess
 from thermox.utils import ProcessedDriftMatrix
 from thermox.utils import ProcessedDiffusionMatrix
