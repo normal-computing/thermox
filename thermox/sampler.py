bd7e3b58b346a1ec05f9dbb4390ea1b9a19cf780
Sam Duffield <s@mduffield.com>
2024-05-31 11:57:03 +0100

    vmap binary operator

--- thermox/sampler.py
+++ thermox/sampler.py
@@ -1,6 +1,6 @@
+from functools import partial
 import jax
 import jax.numpy as jnp
-from jax.lax import scan
 from jax import Array
 
 from thermox.utils import (
@@ -57,22 +57,13 @@ def sample_identity_diffusion(
         out = A.sym_eigvecs @ out
         return out.real
 
-    # def next_x(x, dt, tkey):
-    #     randv = jax.random.normal(tkey, shape=x.shape)
-    #     return transition_mean(x, dt) + transition_cov_sqrt_vp(randv, dt)
-
-    # def scan_body(x_and_key, dt):
-    #     x, rk = x_and_key
-    #     rk, rk_use = jax.random.split(rk)
-    #     x = next_x(x, dt, rk_use)
-    #     return (x, rk), x
-
     dts = jnp.diff(ts)
 
     noise_terms = jax.vmap(transition_cov_sqrt_vp)(
         jax.random.normal(key, (len(dts),) + x0.shape), dts
     )
 
+    @partial(jax.vmap, in_axes=(0, 0))
     def binary_associative_operator(elem_a, elem_b):
         t_a, x_a = elem_a
         t_b, x_b = elem_b
@@ -82,23 +73,8 @@ def sample_identity_diffusion(
     scan_input_values = jnp.concatenate([x0[None], noise_terms], axis=0)
     scan_elems = (scan_times, scan_input_values)
 
-    # elem_a = (scan_elems[0][0], scan_elems[1][0])
-    # elem_b = (scan_elems[0][1], scan_elems[1][1])
-    # elem_c = (scan_elems[0][2], scan_elems[1][2])
-
-    # binary_associative_operator(elem_a, binary_associative_operator(elem_b, elem_c))
-    # binary_associative_operator(binary_associative_operator(elem_a, elem_b), elem_c)
-
     scan_output = jax.lax.associative_scan(binary_associative_operator, scan_elems)
 
-    vals = [(scan_elems[0][0], scan_elems[1][0])]
-    for i in range(len(scan_elems[0])):
-        vals.append(
-            binary_associative_operator(vals[-1], (scan_elems[0][i], scan_elems[1][i]))
-        )
-
-    # xs = scan(scan_body, (x0, key), dts)[1]
-    # xs = jnp.concatenate([jnp.expand_dims(x0, axis=0), xs], axis=0)
     return scan_output[1]
 
 
